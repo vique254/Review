0797a538e567fc7a9cc94bccb7c71fdc28a54f7c
vique254 <victoronyango051@gmail.com>
2020-01-14 17:54:52 +0300

    change view from welcome to home

--- award/urls.py
+++ award/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome')
+    url(r'^$',views.home,name = 'home')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
