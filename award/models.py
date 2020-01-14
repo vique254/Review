669ac3cc26818330cc872c25458261768e899f3e
vique254 <victoronyango051@gmail.com>
2020-01-14 13:05:34 +0300

    create classes in model

--- award/models.py
+++ award/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+   profile_photo=models.ImageField(upload_to='profile',height_field=200,width_field=200,default='')
+   bio = models.CharField(max_length=200)
+   
+class   Image(models.Model):
+    image =models.ImageField(upload_to='home')
+    image_name = models.CharField(max_length=10)
+    image_caption = models.CharField(max_length=250)
+    profile = models.ForeignKey(Profile)
+    likes = models.ManyToManyField('self', symmetrical = False, null = True)
+    comments =models.TextField()
