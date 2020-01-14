8305e7af2f87c645937d6daded35dec39be8029e
vique254 <victoronyango051@gmail.com>
2020-01-14 17:42:17 +0300

    add a def and meta class

--- award/models.py
+++ award/models.py
@@ -5,6 +5,11 @@ class Profile(models.Model):
    profile_photo=models.ImageField(upload_to='profile',height_field=200,width_field=200,default='')
    bio = models.CharField(max_length=200)
    
+   def __str__(self):
+       return self.profile_photo
+   class Meta:
+       ordering = ['profile_photo']
+   
 class   Image(models.Model):
     images =models.ImageField(upload_to='home')
     image_name = models.CharField(max_length=10)
