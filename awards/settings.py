83d49a9f7ec3ecdfef9cd85741a59249f1effad1
vique254 <victoronyango051@gmail.com>
2020-01-14 08:53:02 +0300

    define bootstrap3

--- awards/settings.py
+++ awards/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'award.apps.AwardConfig',
+    'award',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
