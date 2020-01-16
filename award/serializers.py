4eb017721ff7b1c1fcc20418b3f84d8736d8de72
Vique254 <54931914+vique254@users.noreply.github.com>
2020-01-16 11:09:14 +0300

    Update serializers.py

--- award/serializers.py
+++ award/serializers.py
@@ -1,12 +1,1 @@
 from rest_framework import serializers
-from .models import Projects, Profile
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Projects
-        fields = ('id', 'project_image', 'caption', 'project_link', 'user', 'pub_date')
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ('id', 'user', 'profile_photo', 'bio')
